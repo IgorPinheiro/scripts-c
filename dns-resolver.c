a7b78c3a5aa99107c929b0a09b1b6d063d678c10
Igor Pinheiro <igor.warlog@gmail.com>
2020-05-21 17:04:06 -0300

    Update of dns-resolver with error handling.

--- dns-resolver.c
+++ dns-resolver.c
@@ -4,10 +4,22 @@
 #include <stdio.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+
 int main(int argc, char *argv[]){
 	
-	struct hostent *alvo = gethostbyname(argv[1]);
+	if(argc <= 1){
+		printf("Modo de uso: ./dns-resolver sinte.alvo.com.br\n");
+		return 0;
+	}else {
 	
-	printf("IP: %s\n",inet_ntoa(*((struct in_addr *)alvo->h_addr)));
-	return 0;
+		struct hostent *alvo = gethostbyname(argv[1]);
+		
+
+		if (alvo == NULL){
+			printf("Ocorreu um erro :'(\n");
+		} else {
+		
+			printf("IP: %s\n",inet_ntoa(*((struct in_addr *)alvo->h_addr)));
+		}
+}
 }
